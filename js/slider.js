27ca8c57ee749890945a223eb53c13fe8627328e
IhorButov <butovigorg@gmail.com>
2022-09-22 17:46:52 +0300

    add-del

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,16 @@
-const swiper = new Swiper('.MySwiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    // slider gallery
-    centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
